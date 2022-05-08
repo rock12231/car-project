56bfc4391e48616b21b48e8f17e05101c2daa2e6
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-05-09 04:39:10 +0530

    car get data

--- selectCar/views.py
+++ selectCar/views.py
@@ -9,7 +9,15 @@ class Home(View):
     
 class Car(View):
     def get(self, request):
-        return render(request, 'selectCar/car.html')
+        car: dict = {
+            'car_name': 'Toyota',
+            'car_model': 'Corolla',
+            'car_year': '2018',
+            'car_price': '$100,000',
+        }
+        # take input data & send to the template
+        return render(request, 'selectCar/car.html',{'car':car})
     
     def post(self, request):
+        # select car & send to the template with the selected car info
         pass
