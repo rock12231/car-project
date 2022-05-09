32279fc7205fefca965cd8d999fe8c8032eca228
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-05-10 01:24:59 +0530

    comment Code

--- selectCar/views.py
+++ selectCar/views.py
@@ -20,4 +20,13 @@ class Car(View):
     
     def post(self, request):
         # select car & send to the template with the selected car info
+        # csvfile = request.FILES['csv_file']
+        # data = pd.read_csv(csvfile.name)
+        #You can create your custom dataframe here before converting it to html in next line
+        # data_html = data.to_html() 
+        # context = {'loaded_data': data_html}
+        # return render(request, "dataflow/table.html", context)
+        
+        # In the HTML page, use
+        # {{loaded_data | safe}}
         pass
