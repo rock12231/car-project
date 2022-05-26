7219fd6d9dd21906ea6a567a42de89d7043299a8
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-05-26 20:54:26 +0530

    contact us db

--- selectCar/admin.py
+++ selectCar/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from . models import Contact_us
 # Register your models here.
+
+admin.site.register(Contact_us)
--- selectCar/models.py
+++ selectCar/models.py
@@ -1,4 +1,12 @@
 from django.db import models
-
 # Create your models here.
 
+class Contact_us(models.Model):
+    first_name = models.TextField()
+    User_email = models.EmailField(null=False, max_length=254)
+    User_subject = models.TextField(null=False)
+    User_message = models.TextField(null=False, max_length=1000)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.User_email
