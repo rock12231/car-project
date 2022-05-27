8bd4ab973edb7dae2f7a8d06259a473f841876b8
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-05-28 04:08:03 +0530

    working on local

--- cars/settings.py
+++ cars/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from asyncio.windows_events import NULL 
 # import _overlapped
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
