37262b034aa4c822ec8bca007c07f82bd1dc5a49
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-05-28 04:09:40 +0530

    working on local

--- cars/settings.py
+++ cars/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from asyncio.windows_events import NULL 
-# import _overlapped
+import _overlapped
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
