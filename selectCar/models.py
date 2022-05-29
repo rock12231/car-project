380f9f828fc4ea9e098dd4fd0e543d67102c15bc
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-05-29 14:16:41 +0530

    new

--- selectCar/models.py
+++ selectCar/models.py
@@ -2,12 +2,12 @@ from django.db import models
 # Create your models here.
 
 class ContactUs(models.Model):
-    first_name = models.TextField(max_length=50)
-    last_name = models.TextField(max_length=50)
-    User_email = models.EmailField(null=False, max_length=254)
-    User_subject = models.TextField(null=False)
-    User_message = models.TextField(null=False, max_length=1000)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    first_name = models.TextField(max_length=50,default='')
+    last_name = models.TextField(max_length=50,default='')
+    User_email = models.EmailField( max_length=50,default='')
+    User_subject = models.TextField(max_length=100,default='')
+    User_message = models.TextField( max_length=1000,default='')
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.User_email
